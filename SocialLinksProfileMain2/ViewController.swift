cd38a04922385f50a962aa51465005fdf7d0ab04
mfabianchm <mfchongm@hotmail.com>
2024-04-06 10:43:57 -0600

    Initial Commit

---
+++ SocialLinksProfileMain2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocialLinksProfileMain2
+//
+//  Created by Marcos Fabian Chong Megchun on 06/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
